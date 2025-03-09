62859d7dce2cdab16a388917b436eebd3c84af72
Alton <smithiialton@gmail.com>
2025-03-08 23:29:18 -0800

    added event.preventDefault

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 //https://pokeapi.co/
 
 async function searchPokemon(event) {
+    event.preventDefault(); // Prevent form submission
     const searchInput = document.getElementById("search").value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${searchInput.toLowerCase()}`
+    const url = `https://pokeapi.co/api/v2/pokemon/${searchInput.toLowerCase()}`;
     
     try{
-        const response = await fetch(url)
+        const response = await fetch(url);
         if (!response.ok){
-            throw new Error(`Error connecting to Pokemon API: ${response.status}`)
+            throw new Error(`Error connecting to Pokemon API: ${response.status}`);
         }
-        const data = await response.json()
+        const data = await response.json();
         // console.log(data)
 
-        displayPokemon(data)
-
+        displayPokemon(data);
 
     } catch (error) {
-        console.error("Error with Pokemon API:", error )
-        document.getElementById("pokemonInfo").innerHTML = "Error: could not find Pokemon"
+        console.error("Error with Pokemon API:", error);
+        document.getElementById("pokemonInfo").innerHTML = "Error: Could not find Pokémon.";
     }
 }
 
-
 function displayPokemon(data) {
     const pokemonInfo = document.getElementById("pokemonInfo");
-    const types = data.types.map(typeInfo => typeInfo.type.name).join(", ")
+    const types = data.types.map(typeInfo => typeInfo.type.name).join(", ");
 
     pokemonInfo.innerHTML = `
     <h2>${data.name}</h2>
-    <img src="${data.sprites.front_default}"/>
+    <img src="${data.sprites.front_default}" />
     <p>ID: ${data.id}</p>
     <p><strong>Type:</strong> ${types}</p>
-    `
+    `;
 }
 
-document.getElementById("searchForm").addEventListener("submit", searchPokemon)
+document.getElementById("searchForm").addEventListener("submit", searchPokemon);
